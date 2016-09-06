3622e3bcf0de4e0817933be3663c7c19740f335c
Andrew Peterson <peter343@purdue.edu>
2016-09-06 15:53:18 -0400

    Add files via upload

---
+++ McKinseysProgram.java
@@ -0,0 +1,13 @@
+/**
+ * Created by peter343 on 11/17/2015.
+ */
+public class McKinseysProgram {
+    public static void main(String[] args){
+        double i=1;
+
+        while (true){
+            i *= 2;
+            System.out.println(i);
+        }
+    }
+}
